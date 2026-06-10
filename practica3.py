10e37dea42d8e22e60d0e5e01a33a819610ad9d3
roochoa-byte <ro.ochoa@duocuc.cl>
2026-06-10 19:59:48 -0400

    Add files via upload
    Diccionario actualizado con el calculo de promedio de notas finales

---
+++ practica3.py
@@ -0,0 +1,43 @@
+Contacto = {
+    "nombre": input("Ingrese su nombre:"),
+    "telefono": int(input("Ingrese su telefono:")),
+    "email": input("Ingrese su email:"),
+    "Edad": int(input("Ingrese su edad:"))
+}   
+print(Contacto)
+
+while True:
+    print("===MENU====")
+    print("1. Ver ficha de contacto")
+    print("2. Editar ficha de contacto")
+    print("3. Ver promedio de notas")
+    print("4. Salir")
+    opcion = int(input("Ingrese una opcion:"))
+    if opcion == 1:
+        print("Ficha de contacto:")
+        print("Nombre:", Contacto["nombre"])
+        print("Telefono:", Contacto["telefono"])
+        print("Email:", Contacto["email"])
+        print("Edad:", Contacto["Edad"])
+    elif opcion == 2:
+        Contacto["campo"] = input("Ingrese el campo que desea editar: (nombre, telefono, email, edad):")
+        Contacto["valor"] = input("Ingrese el nuevo valor:")
+        if Contacto["campo"] in Contacto:
+            Contacto[Contacto["campo"]] = Contacto["valor"]
+            print("Campo actualizado:", Contacto)
+        else:
+            print("Campo no encontrado.")
+    elif opcion == 3:
+        Notas = []
+        for i in range(5):
+            nota = float(input(f"Ingrese la nota {i+1}: "))
+            Notas.append(nota)
+        suma = sum(Notas)
+        print("Suma de las notas:", suma)
+        promedio = suma / len(Notas)
+        print("Promedio de las notas:", promedio)
+    elif opcion == 4:
+        print("Saliendo del programa...")
+        break
+    else:
+        print("Opcion no valida. Por favor, ingrese una opcion del menu.")
